87ac2f0cebadcaa92751aa2a76271f6e3a291b93
JayKun <junkaiong11@gmail.com>
2018-11-22 00:20:16 +0000

    Add edit feature

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Input, Injectable } from '@angular/core';
 
 export class Post {
   postid: number;
@@ -13,7 +13,7 @@ export class Post {
 })
 
 export class BlogService {
-    posts: Post[];
+    @Input() posts: Post[];
 
     constructor() {}
 
@@ -28,21 +28,6 @@ export class BlogService {
             this.posts = data});
         });
     }
-
-/*    
-    getMaxId(username: string): number {
-        if(typeof this.posts == "undefined"){
-            this.getPosts(username);
-        }
-        let res = 1;
-        for(let post in this.posts){
-            if(post.postid > res) {
-                res = +post.postid;
-            }
-        }
-        return res;
-    }
-*/
 
     getPosts(username: string): Post[] {
         this.fetchPosts(username);
@@ -91,5 +76,72 @@ export class BlogService {
             }
         });
         return null;
+    }
+
+    updatePost(username: string, post: Post): void {
+        let match = null;
+        for(let i in this.posts){
+            if(this.posts[i].postid == post.postid){
+                match = this.posts[i];
+                this.posts[i] = post;
+            }
+        }
+
+        if(!match){
+            return;
+        }
+
+        let newPost = {
+            postid: match.postid,
+            username: match.username,
+            title: post.title,
+            body: post.body,
+            modified: Date.now()
+        };
+
+        let url = "http://192.168.99.100:3000/api/" + username + "/" + match.postid.toString();
+        let p = fetch(url, {
+            method: "PUT",
+            mode: "cors",
+            credentials: "include",
+            body: JSON.stringify(newPost)
+        });
+        
+        p.then( res => {
+            if(res.status == 200){
+                console.log("Update is successful");
+            }
+            else{
+                console.log("Update failed");
+            }
+        });
+    }
+
+    deletePost(username: string, postid: number): void {
+        let match = null;
+        for(let i in this.posts) {
+            if(this.posts[i].postid == postid){
+                match = this.posts[i];
+                delete this.posts[i];
+            }
+        }
+        if(!match){
+            console.log("Post is deleted");
+            return;
+        }
+        let url = "http://192.168.99.100:3000/api/" + username + "/" + postid.toString();
+        let p = fetch(url, {
+            method: "DELETE",
+            mode: "cors",
+            credentials: "include"
+        });
+        p.then( res => {
+            if(res.status == 204){
+                console.log("Error deleting post");
+            }
+            else{
+                console.log("Post deleted");
+            }
+        });
     }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Component, OnInit } from '@angular/core';
 import { BlogService, Post } from '../blog.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-list',
@@ -8,22 +9,33 @@ import { BlogService, Post } from '../blog.service';
 })
 
 export class ListComponent implements OnInit {
-    posts: Post[];
+    @Input() posts: Post[];
 
     constructor(private blogService: BlogService) {
-        this.posts = blogService.getPosts('cs144');
     }
 
     ngOnInit() {
     }
 
-    getPosts(){
-        this.posts = this.blogService.getPosts('cs144');
+    getPosts() {
+        let username = this.parseJWT(document.cookie);
+        this.posts = this.blogService.getPosts(username);
     }
 
     parseJWT(token: string):string {
         let base64Url = token.split('.')[1];
         let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        return JSON.parse(atob(base64));
+        return JSON.parse(atob(base64)).username;
+    }
+
+    getUsernameFromToken(token: string) {
+        const helper = new JwtHelperService();
+        try{
+            return helper.decodeToken(token).usr;
+        }
+        catch(Error){
+            console.log(Error);
+            return null;
+        }
     }
 }
